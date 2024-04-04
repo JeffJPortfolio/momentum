db1501e0ae368500e6d727278a92e4a389851092
DietPepsiBear <jeffjeon.official@gmail.com>
2024-04-05 02:37:32 +0900

    js practice update

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-test
+const h1 = document.querySelector("div.hello:first-child h1") ;
+
+function handleTitleClick() {
+    console.log("title was clicked!") ;
+    h1.style.color = "blue"
+}
+
+function handleMouseEnter() {
+    h1.innerText = "Mouse is here"
+}
+
+function handleMouseLeave() {
+    h1.innerText = "Mouse is gone"
+}
+
+function handleWindowResize() {
+    document.body.style.backgroundColor = "tomato" ;
+}
+
+function handleWindowCopy() {
+    alert("copy!")
+}
+
+//title.addEventListener("click", handleTitleClick) ;
+h1.onclick = handleTitleClick ;
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize) ;
+window.addEventListener("copy", handleWindowCopy);
