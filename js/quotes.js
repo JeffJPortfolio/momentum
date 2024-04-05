6020fad9c27234b207244e6757eb7a4aab2024d1
DietPepsiBear <jeffjeon.official@gmail.com>
2024-04-05 12:02:20 -0400

    clock done

--- js/quotes.js
+++ js/quotes.js
@@ -83,7 +83,7 @@ const quotes = [
     ];
 
 const quote = document.querySelector("#quote span:first-child") ; 
-const author = document.querySelector("#quote span:second-child") ; 
+const author = document.querySelector("#quote span:last-child") ; 
 
 const todaysQuote = quotes[Math.round(Math.random() * quotes.length)] ;
 
