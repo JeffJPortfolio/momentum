59815c92cf1288cd905f762ed5ab7f3c3f26e18d
DietPepsiBear <jeffjeon.official@gmail.com>
2024-04-05 09:50:13 -0400

    login done

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-const loginForm = document.getElementById( "login-form" );
-const loginInput = loginForm.querySelector("input") ;
-const loginButton = loginForm.querySelector("button") ;
+const loginInput = document.querySelector("#login-form input") ;
+const loginForm = document.querySelector("#login-form") ;
+const greeting = document.querySelector("#greeting");
 
-function handLoginButonClick() {
-    const value = loginInput.value ;
-    if (value === "" ) {
-        alert("Please write your name.") ;
-    } else if (value.length > 15){
-        alert("Your name is too long.")
-    } else {
+const HIDDEN_CLASSNAME = "hidden"
+const USERNAME_KEY = "username";
 
-    }
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME) ;
+    const username = loginInput.value ;
+    localStorage.setItem(USERNAME_KEY, username) ;
+    painGreeting(username)
 }
 
+function painGreeting(username) {
+    greeting.innerText = 'Hello ${savedUsername}' 
+    greeting.classList.remove(HIDDEN_CLASSNAME) ;;
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit) ;
+} else {
+    painGreeting(savedUsername)
+
+}
